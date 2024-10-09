3d42f297717f5839083bc73e648bcb8782bbf4c5
PritamBansode13 <pritambansode13@gmail.com>
2024-10-09 10:37:45 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
+ 
+# deploy.sh
+ 
 set -e  # Exit immediately if a command exits with a non-zero status.
-
-# Ensure essential environment variables are set
-if [ -z "$AWS_ACCOUNT_ID" ] || [ -z "$AWS_REGION" ] || [ -z "$AWS_REPOSITORY" ]; then
-  echo "Environment variables AWS_ACCOUNT_ID, AWS_REGION, and AWS_REPOSITORY must be set."
-  exit 1
-fi
-
+ 
+# Define AWS variables
+AWS_ACCOUNT_ID="314806948291"
+AWS_REGION="us-east-2"
+AWS_REPOSITORY="lambda"
+ 
 echo "Pushing the Docker image to Amazon ECR..."
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:latest || {
-  echo "Docker push failed!"
-  exit 1
-}
-
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:latest
+ 
 echo "Creating/Updating Lambda function..."
 aws lambda create-function \
   --function-name randmalay \
@@ -21,12 +20,7 @@ aws lambda create-function \
   --role arn:aws:iam::$AWS_ACCOUNT_ID:role/LambdaExecutionRole \
   --architectures x86_64 \
   --timeout 15 \
-  --memory-size 128 || {
-    echo "Function already exists, updating code..."
-    aws lambda update-function-code \
-      --function-name randmalay \
-      --image-uri $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:latest || {
-        echo "Failed to create or update Lambda function!"
-        exit 1
-      }
-  }
+  --memory-size 128 \
+  || aws lambda update-function-code \
+  --function-name randmalay \
+  --image-uri $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:latest
